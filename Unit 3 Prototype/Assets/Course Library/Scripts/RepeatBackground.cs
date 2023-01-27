dd48b42cc287e01793f5f41ad72b9b7c5794234c
titran3 <121971162+titran3@users.noreply.github.com>
2023-01-27 15:44:39 -0800

    Unit 3

---
+++ Unit 3 Prototype/Assets/Course Library/Scripts/RepeatBackground.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startpos.x - 50)
+        {
+            transform.position = startpos;
+        }
+    }
+}
