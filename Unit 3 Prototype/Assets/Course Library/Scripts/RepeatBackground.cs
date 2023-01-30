e711708015bd892481a2f014eebb14847d54dc68
titran3 <121971162+titran3@users.noreply.github.com>
2023-01-30 14:45:39 -0800

    Lesson 3.3

--- Unit 3 Prototype/Assets/Course Library/Scripts/RepeatBackground.cs
+++ Unit 3 Prototype/Assets/Course Library/Scripts/RepeatBackground.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startpos;
+    private float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startpos.x - 50)
+        if (transform.position.x < startpos.x - repeatWidth)
         {
             transform.position = startpos;
         }
--- Unit 3 Prototype/Assets/Course Library/Scripts/SpawnManager.cs
+++ Unit 3 Prototype/Assets/Course Library/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnpos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,9 @@ public class SpawnManager : MonoBehaviour
         
     }
     void SpawnObstacle (){
+        if (playerControllerScript.gameOver == false)
+        {
         Instantiate(obstaclePrefab, spawnpos, obstaclePrefab.transform.rotation);
+        }
     }
 }
