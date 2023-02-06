1ca8e49af163fb1df77611ad4fed81fe0cff9d8e
titran3 <121971162+titran3@users.noreply.github.com>
2023-02-06 14:21:32 -0800

    Challenge 3 fixed repeating background

--- Unit 3 Prototype/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
+++ Unit 3 Prototype/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class RepeatBackgroundX : MonoBehaviour
 {
-    private Vector3 startPos;
+    private Vector3 startpos;
     private float repeatWidth;
 
-    private void Start()
+    void Start()
     {
-        startPos = transform.position; // Establish the default starting position 
-        repeatWidth = GetComponent<BoxCollider>().size.y / 2; // Set repeat width to half of the background
+        startpos = transform.position; // Establish the default starting position 
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Set repeat width to half of the background
     }
 
-    private void Update()
+    void Update()
     {
         // If background moves left by its repeat width, move it back to start position
-        if (transform.position.x < startPos.x - repeatWidth)
+        if (transform.position.x < startpos.x - repeatWidth)
         {
-            transform.position = startPos;
+            transform.position = startpos;
         }
     }
 
